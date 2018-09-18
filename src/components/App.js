16b08b01602148f5991c17388a197cf1551e4ec2
seungmin <>
2018-09-19 00:13:01 +0900

    immutable, redux-action 적용 작업중

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import PageTemplate from './PageTemplate';
 import TodoInput from './TodoInput';
 import TodoList from './TodoList';
 
-const initialTodos = new Array(500).fill(0).map(
+const initialTodos = new Array(5).fill(0).map(
     (foo, index) => ({id: index, text: `일정 ${index}`, done: false})
 );
 
